f1fc5cd32c932a8fb167ccc7b4e6abb5c82341b1
destinyamba <destiny.amba@gmail.com>
2024-04-23 06:55:26 -0400

    fix get dealers endpoing

--- server/database/app.js
+++ server/database/app.js
@@ -59,7 +59,7 @@ app.get('/fetchReviews/dealer/:id', async (req, res) => {
 // Express route to fetch all dealerships
 app.get('/fetchDealers', async (req, res) => {
     try {
-        const documents = await Reviews.find();
+        const documents = await Dealerships.find();
         res.json(documents);
     } catch (error) {
         res.status(500).json({ error: 'Error fetching documents' });
@@ -80,7 +80,7 @@ app.get('/fetchDealers/:state', async (req, res) => {
 // Express route to fetch dealer by a particular id
 app.get('/fetchDealer/:id', async (req, res) => {
     try {
-        const documents = await Reviews.find({ dealership: req.params.id });
+        const documents = await Dealerships.find({ dealership: req.params.id });
         res.json(documents);
     } catch (error) {
         res.status(500).json({ error: 'Error fetching documents' });
